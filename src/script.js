eca90bd5edf61888e31aa3a9436fef9e81edb00a
Sabera Tehzeeb <88258423+sabera-tehzeeb@users.noreply.github.com>
2024-05-05 17:27:50 +0530

    first commit

--- src/script.js
+++ src/script.js
@@ -1,24 +1,12 @@
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
-buttons.addEventListener("click", (e) => {
-    console.log(e.target.classList);
-    if (e.target.classList.contains("add")) {
-        count.innerHTML++;
-    }
-    if (e.target.classList.contains("subtract")) {
-        count.innerHTML--;
-    }
-    if (e.target.classList.contains("reset")) {
-        count.innerHTML = 0;
-    }
-    setColor();
-});
-function setColor() {
-    if (count.innerHTML > 0) {
-        count.style.color = "greenyellow";
-    } else if (count.innerHTML < 0) {
-        count.style.color = "orangered";
-    } else {
-        count.style.color = "#fff";
-    }
-}
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
+};
+
+btn.addEventListener("click", generateColor);
+
+generateColor();
